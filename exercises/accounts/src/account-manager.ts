a46d7fc6ac9e595b19c4a1fbaa2bc571d5951a2c
pashtunzoy <asifnorzai@outlook.com>
2018-01-21 23:18:58 +1100

    Updating the excerise's code I'm completing

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,5 +1,19 @@
+export interface IUser {
+  email: string;
+  password: string
+}
+
+export interface IConfirmedUser extends IUser {
+  isActive: true
+}
+
+export interface IAdmin extends IConfirmedUser {
+  adminSince: Date
+}
+
+
 export class AccountManager {
-  users = new Array();
+  users :IUser[] = new Array();
 
   /**
    * Create a new user account
@@ -8,7 +22,7 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email :string, password :string) :IUser {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
     let user = { email, password };
@@ -22,10 +36,11 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver :IAdmin, userToApprove :IUser) :IConfirmedUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    let activeUser = userToApprove as IConfirmedUser;
+    activeUser.isActive = true;
+    return activeUser;
   }
 
   /**
@@ -34,10 +49,17 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin :IAdmin, user :IConfirmedUser) :IAdmin {
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    let newAdmin = user as IAdmin;
+    newAdmin.adminSince = new Date();
+    return newAdmin;
   }
 }
+
+let admin: IAdmin = {email: 'test@test.com', password: 'Test123', isActive: true, adminSince: new Date()};
+let user: IConfirmedUser = {email: 'test2@test.com', password: 'Test2123', isActive: true};
+
+let accounts = new AccountManager();
+accounts.promoteToAdmin(admin, user);

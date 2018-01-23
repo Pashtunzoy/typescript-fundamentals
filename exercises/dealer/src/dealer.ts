9e439ed362ae5c0ec9cf748dc7fcd7207e4c5bc4
pashtunzoy <asifnorzai@outlook.com>
2018-01-23 22:11:52 +1100

    Tried to complete this task but no good with cards so typed the solution here instead.

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,63 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+};
 
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades,
+};
+
+type Card = [Suit, CardNumber];
+
+function createDeck(): Card[] {
+  let cards: Card[] = [];
+  for (let s = 0; s < Object.keys(Suit).length; s+=2) {
+    for (let n = 0; n < Object.keys(CardNumber).length; n+=2) {
+      cards.push([s/2, n/2]);
+    }
+  }
+  return cards;
 }
+
+export class Dealer {
+  
+  cards: Card[] = [];
+  constructor() {
+    this.cards = createDeck();
+    shuffleArray(this.cards);
+  
+  }
+  dealHand(numb: number): Card[] {
+    if (numb > this.getLength()) throw new Error("We don't have enough cards left");
+    if (numb < 0) throw new Error("Please insert correct cards");
+    return this.cards.splice(this.getLength() - numb, numb); 
+  }
+  getLength(): number {
+    return this.cards.length;
+  }
+  readCard(card: Card): string {
+    let [suit, cardNumber] = card;
+    return `${CardNumber[cardNumber]} of ${Suit[suit]}`;
+  }
+}
+
+let dealer = new Dealer();
+
+console.log(dealer.dealHand(5));

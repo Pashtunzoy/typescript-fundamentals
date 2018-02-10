134c9afb6d17017d455166e8003fd4d13bac921b
pashtunzoy <asifnorzai@outlook.com>
2018-02-10 20:31:49 +1100

    Finished the exercise

--- exercises/autocomplete-sfc/src/place-search-result.tsx
+++ exercises/autocomplete-sfc/src/place-search-result.tsx
@@ -1,6 +1,16 @@
 import * as React from 'react';
-export const PlaceSearchResult: React.SFC<any> = () => {
+import { PlaceDetails } from './utils/places';
+export const PlaceSearchResult: React.SFC<PlaceDetails> = (pd) => {
   return (
-    <div></div>
+    <li className="search-result">
+      <img src={pd.icon} alt={`${pd.name} image`} className="icon"/>
+      <h3>{pd.name}</h3>
+      {/* <h4>Rating: {pd.rating}</h4> */}
+      <p>
+        <a href={pd.url} target="_blank">{pd.vicinity}</a>
+        -
+        {pd.website && <a href={pd.website} target="_blank">{pd.website}</a>}
+      </p>
+    </li>
   );
 };
